a8206cdb88f1597663d4ecf6119143532fb45220
SWFullPotential <swfullpotential@gmail.com>
2020-09-22 23:00:30 -0600

    Done.

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,8 +3,15 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+  
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    //Should receive a pets prop. This is an array of pets that the component uses to render <Pet /> components. App should determine which pets to pass down as props. App should be responsible for filtering this list based on the types of pets the user wants to see.
+    const pets = this.props.pets.map((pet, index) => (
+      <Pet pet={pet} key={index} onAdoptPet={this.props.onAdoptPet} isAdopted={this.props.adoptedPets.includes(pet.id)} />
+    ))
+    return <div className="ui cards">
+      {pets}
+    </div>
   }
 }
 
