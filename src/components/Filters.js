8e051d2184fa3c96326665fbdde5096a0a6c0882
SWFullPotential <swfullpotential@gmail.com>
2020-09-22 22:50:14 -0600

    Done.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -5,10 +5,10 @@ class Filters extends React.Component {
     super();
     
     //bind this
-    this.handleFilterTypeChange = this.handleFilterTypeChange.bind(this)
+    this.handleChangeTypeFilter = this.handleChangeTypeFilter.bind(this)
   }
-  
-  handleFilterTypeChange(event) {
+  //Should receive an onChangeType callback prop. This callback prop gets called whenever the value of the <select> element changes with the value of the <select>
+  handleChangeTypeFilter(event) {
     this.props.onChangeType(event.target.value);
   }
   
@@ -18,7 +18,7 @@ class Filters extends React.Component {
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type" value={this.props.filters.type} onChangeType={this.handleFilterTypeChange}>
+        <select name="type" id="type" value={this.props.filters.type} onChange={this.handleChangeTypeFilter}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -27,7 +27,7 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button className="ui secondary button" onClick={this.props.onFindPetsClick}>Find pets</button>
         </div>
       </div>
     )
